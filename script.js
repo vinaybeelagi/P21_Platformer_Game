2f7a6fd57f75e387dd9374f986f0ea02090cf0b4
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-30 17:59:05 +0530

    Method to update the player's position and handle collisions

--- script.js
+++ script.js
@@ -27,4 +27,24 @@ class Player {
     ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
   }
   
- 
+  // Method to update the player's position and handle collisions
+  update() {
+    this.draw();
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+
+    if (this.position.y + this.height + this.velocity.y <= canvas.height) {
+      if (this.position.y < 0) {
+        this.position.y = 0;
+        this.velocity.y = gravity;
+      }
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
+
+    if (this.position.x < this.width) {
+      this.position.x = this.width;
+    }
+  }
+}
